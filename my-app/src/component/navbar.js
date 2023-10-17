5fcc5f7dcc5943b22ece985695dafde424669ac5
AtanuG123 <109225904+AtanuG123@users.noreply.github.com>
2023-10-17 23:06:03 +0530

    3rd

--- my-app/src/component/navbar.js
+++ my-app/src/component/navbar.js
@@ -16,7 +16,7 @@ const Navber = () => {
         <div className="container-fluid" >
           <a className="navbar-brand" href="/"><img className="navberlogo" src={require('./profile.jpg')}alt="logo"></img></a>
           <a className="navbar-brand" href="/">Atanu Ghosh</a>
-          <a className="navbar-brand" onClick={()=>changecolor}>change</a>
+          {/* <a className="navbar-brand" onClick={()=>changecolor}>change</a> */}
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -24,7 +24,7 @@ const Navber = () => {
             <ul className="nav navbar-nav">
               <li className="nav-item"><a className="nav-link active" href="#welcome_home">Home</a></li>
               <li className="nav-item"><a className="nav-link active" href="#about">About me</a></li>
-              <li className="nav-item"><a className="nav-link active" href="#myskills">What I Do</a></li>
+              <li className="nav-item"><a className="nav-link active" href="#myskills">My skills</a></li>
               <li className="nav-item"><a className="nav-link active" href="#projects">MY Projects</a></li>
               <li className="nav-item"><a className="nav-link active" href="#contacts">Contact</a></li>
               {/* <li className="nav-item"><a className="nav-link active" href="/technology">Technology</a></li> */}
